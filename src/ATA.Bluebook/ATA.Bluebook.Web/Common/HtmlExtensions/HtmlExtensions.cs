1ddef20b82348a9fbad8e7b212f3009646447a69
chirag <chiragh.itpath@gmail.com>
2024-12-04 12:28:46 +0530

    PR changes for imports and method name

--- src/ATA.Bluebook/ATA.Bluebook.Web/Common/HtmlExtensions/HtmlExtensions.cs
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Common/HtmlExtensions/HtmlExtensions.cs
@@ -4,7 +4,7 @@ namespace ATA.Bluebook.Web.Common.HtmlExtensions
 {
     public static class HtmlExtensions
     {
-        public static string IsActive(this IHtmlHelper html, params string[] controllers)
+        public static string GetActiveRouteClassName(this IHtmlHelper html, params string[] controllers)
         {
             var routeData = html.ViewContext.RouteData;
             var currentController = routeData.Values["controller"]?.ToString();
