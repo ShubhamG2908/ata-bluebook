a20195c7b242193dd7e89ac254fa05c32b60ea25
chirag <chiragh.itpath@gmail.com>
2024-12-10 18:29:19 +0530

    Create generic component for button and added partial view for stepper buttons

---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Common/DxCustomExtensiosns/DxCommonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DevExtreme.AspNet.Mvc;
+using DevExtreme.AspNet.Mvc.Builders;
+using DevExtreme.AspNet.Mvc.Factories;
+
+namespace ATA.Bluebook.Web.Common.DxCustomExtensiosns
+{
+    public static class DxCommonExtensions
+    {
+        public static ButtonBuilder AddCustomButton<TModel>(this WidgetFactory<TModel> factory, string buttonText = "Click", ButtonType buttonType = ButtonType.Default, string? clickHandler = null, string? icon = null)
+        {
+            return factory
+                .Button()
+                .Text(buttonText)
+                .Type(buttonType)
+                .OnClick(clickHandler)
+                .Icon(icon);
+        }
+    }
+}
