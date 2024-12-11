d7e9ca6b85f1c4367394b37df7bbf50cb5be21e8
jenishitpath <jenishp@itpathsolutions.com>
2024-12-11 13:33:54 +0530

    Add: component menu for showing all common components

---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Controllers/ComponentsController.cs
@@ -0,0 +1,57 @@
+﻿using ATA.Bluebook.Web.Models;
+using DevExtreme.AspNet.Data.ResponseModel;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATA.Bluebook.Web.Controllers
+{
+    public class ComponentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(GetComponentTabList());
+        }
+
+        private List<ComponentTabModel> GetComponentTabList()
+        {
+            List<ComponentTabModel> componentTabs = new()
+            {
+                new ComponentTabModel(1, "Textbox"),
+                new ComponentTabModel(2, "Selectbox"),
+            };
+
+            return componentTabs;
+        }
+
+        public IActionResult GetSelectedTabContent(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    {
+                        SelectionBoxModel selectionBoxModel = new SelectionBoxModel()
+                        {
+                            GenderId = 1
+                        };
+                        return PartialView("Editors/_selectboxComponent", selectionBoxModel);
+                    }
+                default:
+                    break;
+            }
+            return PartialView("_selectboxComponent");
+        }
+
+        [HttpGet]
+        public LoadResult GetGenederSelectionData(DataSourceLoadOptions loadOptions)
+        {
+            List<GenderSelectionModel> genderSelectionModel = new List<GenderSelectionModel>();
+            genderSelectionModel.Add(new GenderSelectionModel() { Id = 1, Name = "Male" });
+            genderSelectionModel.Add(new GenderSelectionModel() { Id = 2, Name = "Female" });
+            genderSelectionModel.Add(new GenderSelectionModel() { Id = 3, Name = "Other" });
+            return DataSourceLoader.Load(genderSelectionModel, loadOptions); ;
+        }
+    }
+}
---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/wwwroot/js/Component.js
@@ -0,0 +1,38 @@
+﻿
+function onTabsInitialized(e) {
+    loadPartialView(0);
+}
+
+function selectionChanged(e) {
+    var selectedItem;
+    if (e.selectedItem || e.addedItems?.length) {
+        selectedItem = e.selectedItem || e.addedItems[0]
+    }
+    loadPartialView(selectedItem?.id ?? 0);
+}
+
+function loadPartialView(_id) {
+    $.ajax({
+        url: 'Components/GetSelectedTabContent',
+        method: 'GET',
+        async: true,
+        data: {
+            id: _id
+        },
+        success: function (res) {
+            console.log(res);
+            $("#component-partial-container").empty().append(res);
+        },
+        error: function (res) {
+            console.log(res);
+        }
+    });
+}
+
+function onInitialized(e) {
+    console.log("Init");
+}
+
+function GenderSelectionChanged(e) {
+    console.log(e);
+}
