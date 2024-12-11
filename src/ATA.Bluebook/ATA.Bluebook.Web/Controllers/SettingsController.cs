135688a5fecb9496e1ed6bd3257a971cef8bd117
chirag <chiragh.itpath@gmail.com>
2024-12-11 18:56:34 +0530

    User management init

---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATA.Bluebook.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Users");
+        }
+        
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult ListName()
+        {
+            return View();
+        }
+
+        public IActionResult QuickCostDataEntry()
+        {
+            return View();
+        }
+    }
+}
