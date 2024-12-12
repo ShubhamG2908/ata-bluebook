6ae1b576af43282d073a5ee7582ffbe42c2bd070
jenishitpath <jenishp@itpathsolutions.com>
2024-12-12 14:47:03 +0530

    Implement User Personal Details form

---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Models/UserPersonalDetailFormVM.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ATA.Bluebook.Web.Common.Constants;
+
+namespace ATA.Bluebook.Web.Models
+{
+    public record UserPersonalDetailFormVM
+    (
+        [Required]
+        string Name,
+        [Required]
+        string Address,
+        [Required]
+        string City,
+        [Required]
+        int Zip,
+        [Required]
+        string State,
+        [Required]
+        string Contact,
+        [Required]
+        [RegularExpression(RegexConstants.Email, ErrorMessage = "Email is invalid")]
+        string Email,
+        string? ClosedCode,
+        string? CloseDays,
+        string? WhiteMailCode,
+        bool IsActive
+    );
+
+    public class UserPersonalDetailVM
+    {
+        public UserPersonalDetailFormVM? form { get; set; }
+        public int UserType { get; set; }
+
+        public UserPersonalDetailVM() { }
+
+        public UserPersonalDetailVM(int userType)
+        {
+            UserType = userType;
+        }
+    }
+}
