6fb3fa6fcbf61575fe236b9c7b7fce09bd6a0b09
Shubham Gohel <shubhamg@itpathsolutions.co.in>
2024-12-16 19:24:51 +0530

    - added dxgrid

---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Common/DxCustomExtensiosns/DxDataGridExtensions.cs
@@ -0,0 +1,89 @@
+﻿using ATA.Bluebook.Web.Models.DxConfigs;
+
+using DevExtreme.AspNet.Mvc;
+using DevExtreme.AspNet.Mvc.Builders;
+using DevExtreme.AspNet.Mvc.Builders.DataSources;
+using DevExtreme.AspNet.Mvc.Factories;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ATA.Bluebook.Web.Common.DxCustomExtensiosns
+{
+    public static class DxDataGridExtensions
+    {
+        public static DataGridBuilder<T> AddCustomDxDataGrid<T>(this IHtmlHelper htmlHelper, List<DxDataGridColumnConfig> columnConfigs, DxDataGridConfig gridConfigs)
+        {
+            DataGridBuilder<T> builder = htmlHelper.DevExtreme()
+                                                                             .DataGrid<T>()
+                                                                             .DataSource(cfg => cfg.SetupDataSource(gridConfigs))
+                                                                             .Columns(cfg => cfg.SetupColumns<T>(columnConfigs))
+                                                                             .GroupPanel(cfg => cfg.SetupGroupPanel(gridConfigs.ShowGroupPanel))
+                                                                             .FilterRow(cfg => cfg.Visible(true))
+                                                                             .ColumnHidingEnabled(gridConfigs.AllowColumnHiding)
+                                                                             .Pager(SetupDefaultPager);
+
+            if (gridConfigs.GridEditingConfig.EnableGridEdit)
+            {
+                builder.Editing(cfg =>
+                {
+                    cfg.Mode(GridEditMode.Popup);
+                    cfg.AllowAdding(IsEditActionEnable(gridConfigs.GridEditingConfig.InsertActionName));
+                    cfg.AllowDeleting(IsEditActionEnable(gridConfigs.GridEditingConfig.DeleteActionName));
+                    cfg.AllowUpdating(IsEditActionEnable(gridConfigs.GridEditingConfig.UpdateActionName));
+                    cfg.UseIcons(true);
+                    cfg.Popup(pcfg => pcfg.Title(gridConfigs.GridEditingConfig.PopupTitle));
+                });
+            }
+
+            return builder;
+        }
+
+        #region PRIVATE DATA GRID CONFIG HELPERS
+        private static Action<DataGridPagerBuilder> SetupDefaultPager = (builder) =>
+        {
+            builder.Visible(true);
+            builder.DisplayMode(GridPagerDisplayMode.Compact);
+            builder.ShowPageSizeSelector(true);
+            builder.AllowedPageSizes(new JS("[10, 25, 50, 100]"));
+            builder.ShowInfo(true);
+            builder.ShowNavigationButtons(true);
+        };
+
+        private static void SetupColumns<T>(this CollectionFactory<DataGridColumnBuilder<T>> builder, List<DxDataGridColumnConfig> columnConfigs)
+        {
+            columnConfigs.ForEach(column => builder.Add()
+                                                                               .DataType(column.DataType)
+                                                                               .DataField(column.Field)
+                                                                               .Format(column.Format)
+                                                                               .Caption(column.Caption));
+        }
+
+        private static void SetupGroupPanel(this DataGridGroupPanelBuilder builder, bool showGroupPanel)
+        {
+            builder.Visible(showGroupPanel);
+            builder.EmptyPanelText("Drag a column here for grouping");
+        }
+
+        private static ControllerDataSourceOptionsBuilder SetupDataSource(this DataSourceFactory dataSource, DxDataGridConfig config)
+        {
+            var builder = dataSource.Mvc().
+                Controller(config.DataSourceConfig.ControllerName)
+                .LoadAction(config.DataSourceConfig.LoadActionName)
+                .Key(config.DataSourceConfig.Key);
+
+            if (IsEditActionEnable(config.GridEditingConfig.UpdateActionName))
+                builder.UpdateAction(config.GridEditingConfig.UpdateActionName);
+
+            if (IsEditActionEnable(config.GridEditingConfig.DeleteActionName))
+                builder.UpdateAction(config.GridEditingConfig.DeleteActionName);
+
+            if (IsEditActionEnable(config.GridEditingConfig.InsertActionName))
+                builder.UpdateAction(config.GridEditingConfig.InsertActionName);
+
+            return builder;
+        }
+
+        private static bool IsEditActionEnable(string actionName) => string.IsNullOrWhiteSpace(actionName);
+        #endregion
+    }
+}
---
+++ src/ATA.Bluebook/ATA.Bluebook.Web/Models/DxConfigs/DxDataGridConfigs.cs
@@ -0,0 +1,73 @@
+﻿using DevExtreme.AspNet.Mvc;
+
+namespace ATA.Bluebook.Web.Models.DxConfigs
+{
+    public class DxDataGridColumnConfig
+    {
+        /// <summary>
+        /// DataType of the Property.
+        /// </summary>
+        public required GridColumnDataType DataType { get; set; }
+
+        /// <summary>
+        /// Data Property of the Model.
+        /// </summary>
+        public required string Field { get; set; }
+
+        private string? _caption;
+
+        /// <summary>
+        /// Used for column header. 
+        /// If no value is provided, it will display value of "Field".
+        /// </summary>
+        public string Caption
+        {
+            get { return _caption ?? Field; }
+            set { _caption = value; }
+        }
+
+        /// <summary>
+        /// Indicate the Format type of the Data Property.
+        /// </summary>
+        public Format Format { get; set; }
+
+        /// <summary>
+        /// Only work if the "ColumnHidingEnabled" is set to true.
+        /// Will hide the Column if the value is assigned in the any view.
+        /// </summary>
+        public int? ColumnHidingPriority { get; set; }
+    }
+
+    public class DxDataGridConfig
+    {
+        public DxDataGridConfig()
+        {
+            GridEditingConfig = new();
+        }
+        public required string Id { get; set; }
+        public required DxDataGridDataSourceConfig DataSourceConfig { get; set; }
+        public int PageSize { get; set; } = 20;
+        public bool AllowCoulmnResize { get; set; } = true;
+        public bool DisplayRowFilters { get; set; } = true;
+        public bool AllowColumnReordering { get; set; } = true;
+        public bool AllowColumnHiding { get; set; }
+        public bool ShowGroupPanel { get; set; } = false;
+        public DxDataGridEditingConfig GridEditingConfig { get; set; }
+    }
+
+    public class DxDataGridEditingConfig
+    {
+        public bool EnableGridEdit { get; set; } = false;
+        public string UpdateActionName { get; set; } = default!;
+        public string DeleteActionName { get; set; } = default!;
+        public string InsertActionName { get; set; } = default!;
+        public string PopupTitle { get; set; } = default!;
+    }
+
+    public class DxDataGridDataSourceConfig
+    {
+        public required string ControllerName { get; set; }
+        public required string LoadActionName { get; set; }
+        public string Key { get; set; } = "Id";
+    }
+}
